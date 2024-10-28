1b6a92f9fad34eb61c33f396781be2f3e2c3ef28
tomdeltawork@gmail.com <tomdeltawork@gmail.com>
2024-10-28 21:07:23 +0800

    fix cn function implementation

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,3 +1,6 @@
-export function cn(...classes: string[]) {
-    return classes.filter(Boolean).join(" ");
+// export function cn(...classes: string[]) {
+//     return classes.filter(Boolean).join(" ");
+// }
+export function cn(...classes: (string | boolean | undefined)[]) {
+    return classes.filter((c): c is string => typeof c === "string").join(" ")
 }
